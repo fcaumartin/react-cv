e9001ff921c6f5abb3b9b745e0f587845edd140c
Dan <in.kd@outlook.fr>
2020-11-27 07:12:04 +0100

    Integration template 3-2

--- src/Templates.js
+++ src/Templates.js
@@ -2,15 +2,15 @@ import React, { useState }  from 'react';
 import { DropdownButton, Dropdown } from 'react-bootstrap';
 
 function Templates() {
-    const [style, setStyle] = useState("/template3-5.css");
+    const [style, setStyle] = useState("/template3-2.css");
   
     return (
         <span>
         <link rel="stylesheet" href={style} />
           <DropdownButton variant="secondary"  key={1} id="dropdown-basic-button" title="Modèles" >
-              <Dropdown.Item eventKey="/template3-5.css" onSelect={ (eventKey) => { setStyle(eventKey); }}>Modèle 1</Dropdown.Item>
+              <Dropdown.Item eventKey="/template3-2.css" onSelect={ (eventKey) => { setStyle(eventKey); }}>Modèle 1</Dropdown.Item>
               <Dropdown.Item eventKey="/template2.css" onSelect={ (eventKey) => { setStyle(eventKey); }}>Modèle 2</Dropdown.Item>
-              <Dropdown.Item eventKey="/template3.css" onSelect={ (eventKey) => { setStyle(eventKey); }}>Modèle 3</Dropdown.Item>
+              <Dropdown.Item eventKey="/template3-5.css" onSelect={ (eventKey) => { setStyle(eventKey); }}>Modèle 3</Dropdown.Item>
               <Dropdown.Item eventKey="/template3-5.css" onSelect={ (eventKey) => { setStyle(eventKey); }}>Modèle 4</Dropdown.Item>
           </DropdownButton>
         </span>
