5b365d52e0900ca4e9e72e60629a9d05f0257924
Dan <in.kd@outlook.fr>
2020-11-27 14:00:27 +0100

    Integration template 3-4

--- src/Templates.js
+++ src/Templates.js
@@ -11,7 +11,8 @@ function Templates() {
               <Dropdown.Item eventKey="/template3-2.css" onSelect={ (eventKey) => { setStyle(eventKey); }}>Modèle 1</Dropdown.Item>
               <Dropdown.Item eventKey="/template3-3.css" onSelect={ (eventKey) => { setStyle(eventKey); }}>Modèle 2</Dropdown.Item>
               <Dropdown.Item eventKey="/template3-5.css" onSelect={ (eventKey) => { setStyle(eventKey); }}>Modèle 3</Dropdown.Item>
-              <Dropdown.Item eventKey="/template3-7.css" onSelect={ (eventKey) => { setStyle(eventKey); }}>Modèle 4</Dropdown.Item>
+              <Dropdown.Item eventKey="/template3-4.css" onSelect={ (eventKey) => { setStyle(eventKey); }}>Modèle 4</Dropdown.Item>
+              <Dropdown.Item eventKey="/template3-7.css" onSelect={ (eventKey) => { setStyle(eventKey); }}>Modèle 5</Dropdown.Item>
           </DropdownButton>
         </span>
     );
